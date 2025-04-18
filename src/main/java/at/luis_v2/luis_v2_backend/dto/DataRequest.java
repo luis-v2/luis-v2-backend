0c4162295ac0518af12d1cb389a6afe869e15d0a
Jakob Hanner <jakob.hanner@edu.fh-joanneum.at>
2025-04-18 11:33:13 +0200

    [REF] - Add OpenAPI Examples

--- src/main/java/at/luis_v2/luis_v2_backend/dto/DataRequest.java
+++ src/main/java/at/luis_v2/luis_v2_backend/dto/DataRequest.java
@@ -1,5 +1,6 @@
 package at.luis_v2.luis_v2_backend.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
@@ -13,24 +14,31 @@ import java.util.List;
 public class DataRequest {
 
     @NotBlank(message = "Station must not be blank")
+    @Schema(example = "Graz-Süd", description = "Measurement station name")
     private String station;
 
     @NotEmpty(message = "At least one component must be provided")
+    @Schema(example = "[\"NO2\", \"O3\"]", description = "List of components to retrieve")
     private List<String> components;
 
     @NotNull(message = "Start date is required")
+    @Schema(example = "2025-01-01", description = "Start date in ISO format (yyyy-MM-dd)")
     private LocalDate startDate;
 
     @NotNull(message = "End date is required")
+    @Schema(example = "2025-01-31", description = "End date in ISO format (yyyy-MM-dd)")
     private LocalDate endDate;
 
     @NotBlank(message = "Interval must be specified")
+    @Schema(example = "daily", description = "Interval of the data (e.g., half-hourly, hourly, daily)")
     private String interval;
 
     @NotNull
+    @Schema(example = "false", description = "Should the data be interpolated?")
     private boolean interpolate;
 
     @NotBlank(message = "Format must be specified")
+    @Schema(example = "csv", description = "Format of the exported data")
     private String fileFormat;
 
 }
