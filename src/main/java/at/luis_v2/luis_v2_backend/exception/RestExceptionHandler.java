059615ad30abbee5fd6eb72b70838c209b9df76d
Jakob Hanner <jakob.hanner@edu.fh-joanneum.at>
2025-04-18 11:27:13 +0200

    [FEAT] - Basic REST Exception Handling

---
+++ src/main/java/at/luis_v2/luis_v2_backend/exception/RestExceptionHandler.java
@@ -0,0 +1,22 @@
+package at.luis_v2.luis_v2_backend.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class RestExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleValidationErrors(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getFieldErrors().forEach(error ->
+                errors.put(error.getField(), error.getDefaultMessage()));
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+    }
+}
