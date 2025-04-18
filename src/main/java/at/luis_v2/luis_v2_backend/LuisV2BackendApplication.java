27b2d7f15de0c1cc274a74af36bda50f2288e53c
Jakob Hanner <jakob.hanner@edu.fh-joanneum.at>
2025-04-18 11:26:16 +0200

    [CHORE] - Initial Spring Setup

---
+++ src/main/java/at/luis_v2/luis_v2_backend/LuisV2BackendApplication.java
@@ -0,0 +1,13 @@
+package at.luis_v2.luis_v2_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuisV2BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LuisV2BackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/at/luis_v2/luis_v2_backend/LuisV2BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package at.luis_v2.luis_v2_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LuisV2BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
